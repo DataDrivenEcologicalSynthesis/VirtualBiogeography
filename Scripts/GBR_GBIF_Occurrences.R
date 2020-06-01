29cb72612a1b65674bf895a99967a2f184c1cc07
Amanda Liczner <aliczner@yorku.ca>
2020-06-01 15:33:37 -0400

    add GBR occurrence file

--- Scripts/GBR_GBIF_Occurrences.R
+++ Scripts/GBR_GBIF_Occurrences.R
@@ -3,6 +3,7 @@
 #libraries
 library(readxl) #reads excel files
 library(rgbif) #accesses gbif
+library(plyr) #used to join dataframes with different columns
 
 #read excel file as species list
 GBRsp<-read_excel("Databases/Species_By_Locations/GBR_Species.xlsx") #function to read excel files
@@ -13,10 +14,19 @@ GBR[GBR=="Pygoplites diancathus"]<-"Pygoplites diacanthus" #fixing typos
 GBR<-GBR[!GBR %in% c("Amphiprion ocellaris", "Aetobatus narinari", "Opisthoteuthis californiana", "Forcipiger flavissimus", "Hippocampus kuda", "Paracanthurus hepatus")] #removing focal species that we already pulled
 
 GBRkeys<-sapply(GBR, function(x) name_suggest(x)$key[1], USE.NAMES = FALSE) #function to get the taxonomic key for each species, the taxonic key is a number required by rgbif to lookup each sp in the next step
-GBRspp<-occ_search(taxonKey=paste0(GBRkeys, collapse=";"),hasCoordinate = TRUE, hasGeospatialIssue = FALSE, limit=200000) #extracting occurrence records for focal species - only getting recrods with lat/long and without geospatial issues. 
-#GBIF has a default limit of 500 occurrence records pulled so increasing the limit to the max = 200,000 (we have ~34000 records)
-focaldf<-data.frame(focalsp$data) #converts the output from a tibble to a dataframe, this allows conversion to csv file
+GBRkeys1<-(GBRkeys[1:10]) #there are too many occurrence records to download so dividing the job
+GBRkeys2<-(GBRkeys[11:19])
 
-write.csv(focaldf, "Databases/GBIFOccurrences/FocalSpeciesGBIFFULL.csv") #all columns saved
+GBRspp1<-occ_search(taxonKey=paste0(GBRkeys1, collapse=";"),hasCoordinate = TRUE, hasGeospatialIssue = FALSE, limit=100000) #extracting occurrence records for focal species - only getting recrods with lat/long and without geospatial issues. 
+#GBIF has a default limit of 500 occurrence records pulled so increasing the limit to the max = 100,000
+GBRspp2<-occ_search(taxonKey=paste0(GBRkeys2, collapse=";"),hasCoordinate = TRUE, hasGeospatialIssue = FALSE, limit=100000) #extracting occurrence records for focal species - only getting recrods with lat/long and without geospatial issues. 
 
-focused<-focused<-subset(focaldf[, 2:4])
+GBR1<-data.frame(GBRspp1$data) #converts the output from a tibble to a dataframe, this allows conversion to csv file
+GBR2<-data.frame(GBRspp2$data)
+GBR3<-rbind.fill(GBR1, GBR2) #need rbind.fill because there are differen column names so rbind wont work
+GBR3<-subset(GBR3[,c(1:56, 57, 98)]) #dataset is too large to upload to removed alot of the unnecessary columns
+
+write.csv(GBR3, "Databases/GBIFOccurrences/GBR_GBIFFULL.csv") #all columns saved
+
+focused<-focused<-subset(GBRdf[, 2:4])
+write.csv(focused, "Databases/GBIFOccurrences/GBR_GBIFMinimal.csv")
